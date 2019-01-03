8e2f778d9ad011c88cfc13c541a634d676378c43
Alexis David Hurtado Garcia <Alexis@localhost.localdomain>
2019-01-03 14:03:57 -0600

    Camel Web Page

---
+++ src/main/java/com/nuuptech/springinvokescamel/DummyRouteBuilder.java
@@ -0,0 +1,30 @@
+package com.nuuptech.springinvokescamel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+public class DummyRouteBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("direct:blogEntryTest")
+			.process(new Processor() {
+
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					String message = exchange.getIn().getBody(String.class);
+					int lettersCount = 0;
+					
+					if (message != null) {
+						lettersCount = message.length();
+					}
+					
+					exchange.getOut().setBody(lettersCount);
+				}
+				
+			})
+		.end();
+	}
+
+}
---
+++ src/main/java/com/nuuptech/springinvokescamel/Service.java
@@ -0,0 +1,25 @@
+package com.nuuptech.springinvokescamel;
+
+import spark.servlet.SparkApplication;
+import static spark.Spark.*;
+
+public class Service implements SparkApplication {
+
+	@Override
+	public void init() {
+		
+		get("/ping", (req, res) -> {
+			return "pong";
+		});
+		
+		get("/api/count", (req, res) -> {
+			String input = req.queryParams("input");
+			Object count = CamelCaller.getInstance().callCamelRoute("direct:blogEntryTest", input);
+			int countLength = Integer.parseInt(count.toString());
+			
+			return String.format("'%s' lenght: %d", input, countLength);
+		});
+		
+	}
+
+}
